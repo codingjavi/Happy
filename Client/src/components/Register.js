76da07a79e45830316bd9d455ec89bf8bbcbd5da
Javier <pacheco.javi01@gmail.com>
2024-01-18 13:04:15 -0600

    adding and setting up register component

--- Client/src/components/Register.js
+++ Client/src/components/Register.js
@@ -1,4 +1,21 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+//regex: how we're validating user and password
+  //[]: start with lower case or upper case a - z
+  //2nd []: can be followed by upper or lower a -z and 0-9 or - _
+  //{}: has to have 4 - 23 chars
+//password:
+  //(): has to have atleast one lowercase
+  //(): has to heve atleast one upper case
+  //(): has to have one of these special characters
+  //{}: 9 - 24 chars long
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/register'; //to make api call to backend
+
 
 function Register() {
   return (
